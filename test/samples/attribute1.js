78ca0b5f1c69f1bed30b53c9a89d0f734125dc6e
Sergey Chikuyonok <serge.che@gmail.com>
2018-09-17 23:46:33 +0300

    Code refactoring

--- test/samples/attribute1.js
+++ test/samples/attribute1.js
@@ -12,27 +12,49 @@ export default function(component, target = component) {
 	const injector = createInjector(elem1);
 	const attr1 = renderAttribute(scope, injector, 'a1', attrValue1);
 	renderAttribute(scope, injector, 'a2', '0');
-	const attr2 = renderAttribute(scope, injector, 'a2', '1', attrCond1);
-	const attr3 = renderAttribute(scope, injector, 'a2', '2', attrCond2);
+
+	// <attribute if="..."> is the same as <if test="..."><attribute /></if>
 	const block1 = renderBlock(scope, injector, ifBlock1);
+	const block2 = renderBlock(scope, injector, ifBlock2);
+	const block3 = renderBlock(scope, injector, ifBlock3);
 	const attr4 = renderAttribute(scope, injector, 'a3', '4');
 
 	return () => {
 		attr1();
-		attr2();
-		attr3();
 		block1();
+		block2();
+		block3();
 		attr4();
 	};
 }
 
 function ifBlock1(scope) {
-	if (getProp(scope, 'c3')) {
+	if (getProp(scope, 'c1')) {
 		return ifContent1;
 	}
 }
 
 function ifContent1(scope, injector) {
+	renderAttribute(scope, injector, 'a2', '1');
+}
+
+function ifBlock2(scope) {
+	if (getProp(scope, 'c2')) {
+		return ifContent2;
+	}
+}
+
+function ifContent2(scope, injector) {
+	renderAttribute(scope, injector, 'a2', '2');
+}
+
+function ifBlock3(scope) {
+	if (getProp(scope, 'c3')) {
+		return ifContent3;
+	}
+}
+
+function ifContent3(scope, injector) {
 	renderAttribute(scope, injector, 'a2', '3');
 	renderAttribute(scope, injector, 'a1', '3');
 	renderAttribute(scope, injector, 'a3', '3');
@@ -40,12 +62,4 @@ function ifContent1(scope, injector) {
 
 function attrValue1(scope) {
 	return getProp(scope, 'id');
-}
-
-function attrCond1(scope) {
-	return getProp(scope, 'c1');
-}
-
-function attrCond2(scope) {
-	return getProp(scope, 'c2');
 }
