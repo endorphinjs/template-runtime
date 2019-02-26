ad5c6680dc52eb32758e95bc9a0ff07d2581cd64
Sergey Chikuyonok <serge.che@gmail.com>
2019-02-26 13:14:23 +0300

    Fixed props update implementation

--- test/props.js
+++ test/props.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import document from './assets/document';
 import template from './samples/props';
-import { createComponent, mountComponent, renderComponent } from '../runtime';
+import { createComponent, mountComponent, renderComponent, updateComponent, setAttribute} from '../runtime';
 
 describe('Props', () => {
 	before(() => global.document = document);
@@ -28,5 +28,22 @@ describe('Props', () => {
 
 		component.setProps({ c1: false });
 		assert.deepEqual(sub.props, { p1: 1, id: 'bar', p3: 3, p2: null });
+	});
+
+	it('should init with default props', () => {
+		const component = createComponent('my-component', {
+			default: template,
+			props() {
+				return { id: 'foo', c1: false };
+			}
+		});
+
+		setAttribute(component.componentModel.input, 'c1', true);
+		mountComponent(component, { id: 'bar' });
+		assert.deepEqual(component.props, { id: 'bar', c1: true });
+
+		setAttribute(component.componentModel.input, 'c1', false);
+		updateComponent(component);
+		assert.deepEqual(component.props, { id: 'bar', c1: false });
 	});
 });
