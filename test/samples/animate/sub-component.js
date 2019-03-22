3a6a1b2f62a2ba39a6783720798ef46f62ba4b71
Sergey Chikuyonok <serge.che@gmail.com>
2019-03-22 17:43:26 +0300

    Support callbacks on animation end

--- test/samples/animate/sub-component.js
+++ test/samples/animate/sub-component.js
@@ -1,8 +1,18 @@
-import { text } from "../../../dist/runtime.es.js";
+import { elem, insert, text, mountSlot, unmountSlot, addDisposeCallback } from "../../../dist/runtime.es.js";
 
-export default function $$template0(host) {
+export default function $$template0(host, scope) {
 	const target0 = host.componentView;
-	target0.appendChild(text("Sub component"));
+	const slot0 = target0.appendChild(elem("slot"));
+	scope.$_slot0 = mountSlot(host, "", slot0, $$slotDefaultContent0);
+	addDisposeCallback(host, $$template0Unmount);
+}
+
+function $$template0Unmount(scope) {
+	scope.$_slot0 = unmountSlot(scope.$_slot0);
+}
+
+function $$slotDefaultContent0(host, injector) {
+	insert(injector, text("Sub component"));
 }
 
 
